915fa58fe48b0788d656871444962f1a20e7b2d9
Rin-Kokubo <86641034+RinKokubo@users.noreply.github.com>
2023-06-28 22:46:10 +0900

    add attacker card db

--- database/seeders/AttackerCardSeeder.php
+++ database/seeders/AttackerCardSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\AttackerCard;
 use Illuminate\Database\Seeder;
 
 class AttackerCardSeeder extends Seeder
@@ -13,6 +14,32 @@ class AttackerCardSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $cardNames = [
+            'オレオレ詐欺',
+            '代引き詐欺',
+            'スパムメール',
+            'アカウント乗っ取り',
+            'ストーカー',
+            '空き巣',
+            '宗教勧誘',
+            '年金詐欺',
+            '国際ロマンス詐欺',
+            'SNSなりすまし',
+            '代引き詐欺',
+            'フィッシング詐欺',
+            'いたずら電話',
+            'ECサイトアカウント乗っ取り',
+            '預貯金詐欺',
+            '架空請求',
+            '結婚詐欺',
+            '金銭的略取',
+            '勧誘電話',
+        ];
+
+        foreach ($cardNames as $name) {
+            AttackerCard::create([
+                'attacker_card_name' => $name,
+            ]);
+        }
     }
 }
