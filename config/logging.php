e42001cac0dcee9e2b8e6f05989868c419ffecc1
Rin-Kokubo <86641034+RinKokubo@users.noreply.github.com>
2023-07-03 00:10:39 +0900

    show score

--- config/logging.php
+++ config/logging.php
@@ -50,7 +50,7 @@ return [
     'channels' => [
         'stack' => [
             'driver' => 'stack',
-            'channels' => ['single'],
+            'channels' => ['single', 'stderr'],  // 'stderr' を追加
             'ignore_exceptions' => false,
         ],
 
