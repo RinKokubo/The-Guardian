7a0b5c21560defe4d64ae2fb6b85f7e5c6fdd290
Rin-Kokubo <86641034+RinKokubo@users.noreply.github.com>
2023-07-04 17:24:42 +0900

    fix

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -15,84 +15,178 @@ class UserSeeder extends Seeder
     public function run()
     {
         User::create([
-            'name' => 'zemi1',
+            'name' => 'user101',
             'password' => 11111,
             'is_logged_in' => 1,
         ]);
 
         User::create([
-            'name' => 'zemi2',
+            'name' => 'user102',
             'password' => 11112,
             'is_logged_in' => 1,
         ]);
         User::create([
-            'name' => 'zemi3',
+            'name' => 'user103',
             'password' => 11113,
             'is_logged_in' => 1,
         ]);
 
         User::create([
-            'name' => 'zemi4',
+            'name' => 'user104',
             'password' => 11114,
             'is_logged_in' => 1,
         ]);
 
         User::create([
-            'name' => 'zemi5',
+            'name' => 'user105',
             'password' => 11115,
             'is_logged_in' => 1,
         ]);
 
         User::create([
-            'name' => 'zemi6',
+            'name' => 'user106',
             'password' => 11116,
             'is_logged_in' => 1,
         ]);
 
         User::create([
-            'name' => 'zemi7',
+            'name' => 'user107',
             'password' => 11117,
             'is_logged_in' => 1,
         ]);
 
         User::create([
-            'name' => 'zemi8',
+            'name' => 'user108',
             'password' => 11118,
             'is_logged_in' => 1,
         ]);
         User::create([
-            'name' => 'zemi9',
+            'name' => 'user109',
             'password' => 11119,
             'is_logged_in' => 1,
         ]);
 
         User::create([
-            'name' => 'zemi10',
+            'name' => 'user110',
             'password' => 111110,
             'is_logged_in' => 1,
         ]);
 
         User::create([
-            'name' => 'zemi11',
+            'name' => 'user111',
             'password' => 111111,
             'is_logged_in' => 1,
         ]);
 
         User::create([
-            'name' => 'zemi12',
+            'name' => 'user112',
             'password' => 111112,
             'is_logged_in' => 1,
         ]);
 
         User::create([
-            'name' => 'zemi13',
+            'name' => 'user113',
             'password' => 111113,
             'is_logged_in' => 1,
         ]);
 
         User::create([
-            'name' => 'zemi14',
+            'name' => 'user114',
             'password' => 111114,
+            'is_logged_in' => 1,
+        ]);
+
+        User::create([
+            'name' => 'user115',
+            'password' => 111115,
+            'is_logged_in' => 1,
+        ]);
+
+        User::create([
+            'name' => 'user116',
+            'password' => 111116,
+            'is_logged_in' => 1,
+        ]);
+
+        User::create([
+            'name' => 'user117',
+            'password' => 111117,
+            'is_logged_in' => 1,
+        ]);
+
+        User::create([
+            'name' => 'user118',
+            'password' => 111118,
+            'is_logged_in' => 1,
+        ]);
+        User::create([
+            'name' => 'user119',
+            'password' => 111119,
+            'is_logged_in' => 1,
+        ]);
+
+        User::create([
+            'name' => 'user120',
+            'password' => 111120,
+            'is_logged_in' => 1,
+        ]);
+
+        User::create([
+            'name' => 'user121',
+            'password' => 111121,
+            'is_logged_in' => 1,
+        ]);
+
+        User::create([
+            'name' => 'user122',
+            'password' => 111122,
+            'is_logged_in' => 1,
+        ]);
+
+        User::create([
+            'name' => 'user123',
+            'password' => 111123,
+            'is_logged_in' => 1,
+        ]);
+
+        User::create([
+            'name' => 'user124',
+            'password' => 111124,
+            'is_logged_in' => 1,
+        ]);
+
+        User::create([
+            'name' => 'user125',
+            'password' => 111125,
+            'is_logged_in' => 1,
+        ]);
+
+        User::create([
+            'name' => 'user126',
+            'password' => 111126,
+            'is_logged_in' => 1,
+        ]);
+
+        User::create([
+            'name' => 'user127',
+            'password' => 111127,
+            'is_logged_in' => 1,
+        ]);
+
+        User::create([
+            'name' => 'user128',
+            'password' => 111128,
+            'is_logged_in' => 1,
+        ]);
+        User::create([
+            'name' => 'user129',
+            'password' => 111129,
+            'is_logged_in' => 1,
+        ]);
+
+        User::create([
+            'name' => 'user130',
+            'password' => 111130,
             'is_logged_in' => 1,
         ]);
     }
