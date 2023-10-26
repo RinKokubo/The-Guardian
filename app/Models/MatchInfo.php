19d558141c8d88202cdf6f01e814741510cb4fbb
Rin-Kokubo <86641034+RinKokubo@users.noreply.github.com>
2023-10-26 21:03:59 +0900

    add match info db

---
+++ app/Models/MatchInfo.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class MatchInfo extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'game_id',
+        'opponent_id',
+        'user_role'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function game()
+    {
+        return $this->belongsTo(GameInformation::class, 'game_id');
+    }
+
+    public function opponent()
+    {
+        return $this->belongsTo(User::class, 'opponent_id');
+    }
+}
---
+++ database/migrations/2023_10_26_204834_create_match_info_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMatchInfoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('match_info', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('game_id');
+            $table->unsignedBigInteger('opponent_id');
+            $table->string('user_role');
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('game_id')->references('id')->on('game_information');
+            $table->foreign('opponent_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('match_info');
+    }
+}
